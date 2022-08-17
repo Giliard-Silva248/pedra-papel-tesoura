195d06ea0dc6de45f83a269b8ca3bc9000cc5906
Giliard H. da Silva <ghssilva248@gmail.com>
2022-08-16 22:19:39 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,79 +1,63 @@
 let res = document.querySelector('.res')
-let btn = document.querySelector('#btn')
-
-let pe = document.querySelector('#ipedra').value
-let pa = document.querySelector('#ipapel').value
-let te = document.querySelector('#itesoura').value
-
-
+let maquina = document.querySelector('.maquina')
 /*
 PEDRA = 1
 PAPEL = 2
 TESOURA = 3
 */
-
-
-
-
-
-
-let ale = Math.floor(Math.random() * (4 - 1) + 1)
-function jogo(){
+function pedra(){
+    let pe = document.querySelector('#ipedra')
     
-    function pedra(){
-        console.log(ale);
-    
-        if(pe){
-            if(ale === 1){
-                 res.innerHTML = 'empartado'
-             } else if(ale === 2){
-                 res.innerHTML = 'perdeu'
-             } else if(ale === 3){
-                 res.innerHTML = 'ganhou'
-             }
-          jogo()
-        }
+    let alePe = Math.floor(Math.random() * (4 - 1) + 1)
+    if(pe){
         
+        if(alePe == 1){
+             res.textContent = `O RESULTADO: Você empatou!`
+             maquina.innerHTML ='<img src="css/pedra.jpg" alt="">'
+        }
+        if(alePe == 2){
+             res.textContent = `O RESULTADO: Você perdeu!`
+             maquina.innerHTML ='<img src="css/papel.jpg" alt="">'
+        }
+        if(alePe == 3){
+             res.textContent = `O RESULTADO: Você ganhou!`
+             maquina.innerHTML ='<img src="css/tesoura.jpg" alt="">'
+        }
     }
 }
-
-
-
-
 function papel(){
-
+    let pa = document.querySelector('#ipapel')
+    let alePa = Math.floor(Math.random() * (4 - 1) + 1)
     if(pa){
-        if(ale === 1 ){
-            res.innerHTML = 'ganhou'
-        } else if(ale === 2){
-            res.innerHTML = 'empartado'
-        }else if(ale === 3){
-            res.innerHTML = 'perdeu'
+        if(alePa == 1){
+             res.textContent = `O RESULTADO: Você ganhou!`
+             maquina.innerHTML ='<img src="css/pedra.jpg" alt="">'
         }
-    } 
+        if(alePa == 2){
+             res.textContent = `O RESULTADO: Você empatou!`
+             maquina.innerHTML ='<img src="css/papel.jpg" alt="">'
+        }
+        if(alePa == 3){
+             res.textContent = `O RESULTADO: Você perdeu!`
+             maquina.innerHTML ='<img src="css/tesoura.jpg" alt="">'
+        }
+    }
 }
 function tesoura(){
-
+    let te = document.querySelector('#itesoura')
+    let aleTe = Math.floor(Math.random() * (4 - 1) + 1)
     if(te){
-        
-        if(ale == 1){
-            res.innerHTML = 'perdeu'
-           
-        } else if(ale == 2){
-            res.innerHTML = 'ganhou'
-          
-        }else if(ale == 3){
-            res.innerHTML = 'empartado'
-           
+        if(aleTe == 1){
+             res.textContent = `O RESULTADO: Você perdeu!`
+             maquina.innerHTML ='<img src="css/pedra.jpg" alt="">'
+        }
+        if(aleTe == 2){
+             res.textContent = `O RESULTADO: Você ganhou!`
+             maquina.innerHTML ='<img src="css/papel.jpg" alt="">'
+        }
+        if(aleTe == 3){
+             res.textContent = `O RESULTADO: Você empatou!`
+             maquina.innerHTML ='<img src="css/tesoura.jpg" alt="">'
         }
     }
 }
-
-
-    
-
-    
-    
-    
-  
-    
